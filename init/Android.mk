290b542ef57bc85695c96482819ac8bba158abe9
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2024-10-06 18:40:24 +0200

    Delete init directory

--- init/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
-LOCAL_SRC_FILES := init_$(BOARD_VENDOR_PLATFORM).cpp
-LOCAL_MODULE := libinit_$(BOARD_VENDOR_PLATFORM)
-include $(BUILD_STATIC_LIBRARY)
