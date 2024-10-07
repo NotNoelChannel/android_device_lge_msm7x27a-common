a1cda8bacad8290fbfe3528911243a06a206b59d
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2024-10-07 18:10:14 +0200

    Oopsies, fix issue

--- product/input.mk
+++ product/input.mk
@@ -1,8 +1,8 @@
 # Common Keys layouts
 PRODUCT_COPY_FILES += \
-    device/lge/msm7x27a-common/rootdir/system/usr/keylayout/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
-    device/lge/msm7x27a-common/rootdir/system/usr/keylayout/7x27a_kp.kl:system/usr/keylayout/7x27a_kp.kl
+    device/lge/e0-common/rootdir/system/usr/keylayout/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
+    device/lge/e0-common/rootdir/system/usr/keylayout/7x27a_kp.kl:system/usr/keylayout/7x27a_kp.kl
 
 # Device Keys layouts
 PRODUCT_COPY_FILES += \
-    device/lge/$(LGE_MSM7X27A_DEVICE)/rootdir/system/usr/keylayout/$(BOARD_VENDOR_PLATFORM)_keypad.kl:system/usr/keylayout/$(BOARD_VENDOR_PLATFORM)_keypad.kl
+    device/lge/e0/rootdir/system/usr/keylayout/e400_keypad.kl:system/usr/keylayout/e400_keypad.kl
