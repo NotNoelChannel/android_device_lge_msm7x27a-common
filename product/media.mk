981800c990b76721a8ce1f17eedc231a930f1b54
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2024-10-07 18:11:44 +0200

    Update media.mk

--- product/media.mk
+++ product/media.mk
@@ -1,7 +1,7 @@
 # Media common configurations
 PRODUCT_COPY_FILES += \
-    device/lge/msm7x27a-common/rootdir/system/etc/media_codecs.xml:system/etc/media_codecs.xml
+    device/lge/e0-common/rootdir/system/etc/media_codecs.xml:system/etc/media_codecs.xml
 
 # Media device configurations
 PRODUCT_COPY_FILES += \
-    device/lge/msm7x27a-common/rootdir/system/etc/media_profiles.xml:system/etc/media_profiles.xml
+    device/lge/e0-common/rootdir/system/etc/media_profiles.xml:system/etc/media_profiles.xml
